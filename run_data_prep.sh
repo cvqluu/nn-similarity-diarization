54464c87d2c2bd4c317d55e18c10f5659a179e81
Chau Luu <s1786813@sms.ed.ac.uk>
2019-11-14 14:34:46 +0000

    xvec data prep script

---
+++ run_data_prep.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# mostly adapted from original run.sh in 
+# https://github.com/kaldi-asr/kaldi/blob/master/egs/callhome_diarization/v2/run.sh
+
+stage=0
+nnet_dir=0006_callhome_diarization_v2_1a/exp/xvector_nnet_1a
+nj=20
+train_cmd=run.pl
+callhome_path= #path to raw callhome data
+xvector_dir= #path to extracted xvectors
+
+if [ $stage -le 0 ]; then
+    local/make_callhome.sh $callhome_path data
+    utils/combine_data.sh data/callhome data/callhome1 data/callhome2
+fi
+
+if [ $stage -le 1 ]; then
+    steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj $nj \
+      --cmd "$train_cmd" --write-utt2num-frames true \
+      data/callhome exp/make_mfcc mfcc
+    utils/fix_data_dir.sh data/callhome
+
+    sid/compute_vad_decision.sh --nj $nj --cmd "$train_cmd" \
+      data/callhome exp/make_vad mfcc
+    utils/fix_data_dir.sh data/callhome
+
+    for name in callhome; do
+        local/nnet3/xvector/prepare_feats.sh --nj 40 --cmd "$train_cmd" \
+        data/$name data/${name}_cmn exp/${name}_cmn
+        cp data/$name/vad.scp data/${name}_cmn/
+        if [ -f data/$name/segments ]; then
+            cp data/$name/segments data/${name}_cmn/
+        fi
+        utils/fix_data_dir.sh data/${name}_cmn
+    done
+fi
+
+if [ $stage -le 2 ]; then
+    # download pretrained kaldi model
+    wget https://kaldi-asr.org/models/6/0006_callhome_diarization_v2_1a.tar.gz
+    tar -xvf 0006_callhome_diarization_v2_1a.tar.gz
+fi
+
+if [ $stage -le 3 ]; then
+    diarization/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd" \
+        --nj $nj --window 1.5 --period 0.75 --apply-cmn false \
+        --min-segment 0.5 $nnet_dir \
+        data/callhome_cmn $nnet_dir/xvectors_callhome
+fi
